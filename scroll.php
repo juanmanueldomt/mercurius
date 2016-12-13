367a0fcf911504551ecde9fbb4e753742160066e
Juan Manuel Dominguez Tlapale <cronoz.v@gmail.com>
2016-12-13 17:29:51 -0600

    version 0.0.4

--- scroll.php
+++ scroll.php
@@ -15,28 +15,30 @@ function showData($data,$con,$limit){
   $start = ($page-1)*$limit;
   }
   
-  $sql = "select * from NOTICIA limit $start,$limit";
+  $sql = "SELECT * FROM NOTICIA LIMIT $start,$limit";
   $str='';
+    $cont=0;
   $data = $con->query($sql);
   if($data!=null && $data->num_rows>0){
    while( $row = $data->fetch_array(MYSQLI_ASSOC)){
     //  $str.="<div class='data-container'><p>".$row['ID_NOTICIA']."</p><p>".$row['AUTOR']."</p><p>".$row['CONTENIDO']."</p></div>";
-            echo'<div class="media data-container">';
-            echo'<div class="media-left">';
-            echo'<img src="resources/images/ColoresUpiicsa.jpg" class="media-object">';
-            echo'</div>';
-            echo'<div class="media-body">';
-            echo'<h4 class="media-heading">'.$row[TITULO].'</h4>';
-            echo'<p>'.substr($row[CONTENIDO],0,70).'...</p>';
-            echo'</div>';
-            echo'</div>';     
        
-            
-       
+       echo '<hr class="featurette-divider">';
+            echo'<div class="row featurette">';
+            echo'<div class="col-md-7">';
+            echo'<h2 class="featurette-heading">'.$row[TITULO].'</h2>';
+            echo'<p class="lead">'.substr($row[CONTENIDO],0,240).'</p>';
+            echo'</div>';
+            echo'<div class="col-md-5">';
+            echo'<img class="featurette-image img-responsive" src="resources/images/ColoresUpiicsa.jpg" alt="Generic placeholder image">';
+            echo'</div>';
+        echo'</div>';
+
    }
    $str.="<input type='hidden' class='nextpage' value='".($page+1)."'><input type='hidden' class='isload' value='true'>";
    }else{
-    $str .= "<input type='hidden' class='isload' value='false'><p>Finished</p>";
+      $str .='<hr class="featurette-divider">';
+    $str .= "<input type='hidden' class='isload' value='false'><center><p>Finished</p></center>";
    }
   
    
