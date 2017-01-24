92ff10d631697d47f7c05d4747d801aa733a157e
Juan Manuel Dominguez Tlapale <cronoz.v@gmail.com>
2017-01-24 15:02:11 -0600

    version 0.0.4.3

--- db.php
+++ db.php
@@ -6,8 +6,16 @@
     $dbname = "mercurius";
 
     $con = mysqli_connect($servername,$username,$password,$dbname);
-    if(mysqli_connect_errno()){
-        echo "Error en la conexion de base de datos";
-        exit();
-    }
+
+    if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+if (!mysqli_query($link, "SET a=1")) {
+    printf("Errormessage: %s\n", mysqli_error($con));
+}
+
+/* Cierra la conexión */
+mysqli_close($con);
 ?>
